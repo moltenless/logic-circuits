b193c68d7e5861c0ccaa42ef76bb93f0c28e9762
Serhii Turlak <turlakserhii@gmail.com>
2024-07-14 13:23:57 +0300

    implemented all variety of template's parameters

--- Forms/TruthTableForm.cs
+++ Forms/TruthTableForm.cs
@@ -16,19 +16,52 @@ namespace LogicCircuits.Forms
             {
 
             };
+
+
+            (IElement, int outputResult)[] copiedRegistry = new (IElement, int outputResult)[registry.Count];
+            registry.CopyTo(copiedRegistry);
+
             int inputsCount = 0;
-            List<Input> inputs = new List<Input>();
-            string text = string.Empty;
-            for (int i = 0; i < registry.Count; i++)
+            List<(Input input, int value)> inputs = new List<(Input, int)>();
+            (Output output, int result) output = (copiedRegistry.Last().Item1 as Output, copiedRegistry.Last().outputResult);
+
+            for (int i = 0; i < copiedRegistry.Length; i++)
             {
-                if (registry[i].Item1 is Input)
+                if (copiedRegistry[i].Item1 is Input)
                 {
                     inputsCount++;
-                    inputs.Add((Input)registry[i].Item1);
-                    text += inputs.Last().Name + ", ";
+                    inputs.Add(((Input)copiedRegistry[i].Item1, copiedRegistry[i].outputResult));
                 }
             }
-            MessageBox.Show(inputsCount.ToString() + " counted, " + text);
+
+            bool[,] parameters = new bool[(int)Math.Pow(2, inputsCount), inputsCount];
+
+            for (int i = 0; i < inputsCount; i++)
+            {
+                int groups = (int)Math.Pow(2, i + 1);
+                int groupsBy = (int)Math.Pow(2, inputsCount) / groups;
+
+                for (int j = 0; j < groups; j++)
+                {
+                    for (int k = 0; k < groupsBy; k++)
+                    {
+                        if (j % 2 == 1)
+                            parameters[j * groupsBy + k, i] = true;
+                        else if (j % 2 == 1)
+                            parameters[j * groupsBy + k, i] = false;
+                    }
+                }
+            }
+
+            string text = string.Empty;
+            for (int i = 0; i < (int)Math.Pow(2, inputsCount); i++)
+            {
+                for (int j = 0; j < inputsCount; j++)
+                    text += parameters[i, j] ? "1\t" : "0\t";
+                text += "\n";
+            }
+
+            MessageBox.Show(text);
 
             return form;
         }
