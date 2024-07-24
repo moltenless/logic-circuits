5f0fe026155858ea32d1b6358c2c91562692e8ef
Serhii Turlak <turlakserhii@gmail.com>
2024-07-24 13:06:05 +0300

    DNF form is done

--- Forms/DNFForm.cs
+++ Forms/DNFForm.cs
@@ -24,10 +24,10 @@ namespace LogicCircuits.Forms
                 Text = "ДДНФ",
                 Font = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold),
                 Tag = (truthTable, columnNames),
-                Width = 450,
+                Width = 750,
                 Height = 151,
                 FormBorderStyle = FormBorderStyle.FixedSingle,
-                MaximizeBox = false, 
+                MaximizeBox = false,
                 AutoScroll = true,
             };
 
@@ -40,6 +40,42 @@ namespace LogicCircuits.Forms
                 ScrollBars = ScrollBars.Vertical,
             };
             form.Controls.Add(textBox);
+
+            string function = $"{columnNames[cols - 1]}({columnNames[0]}";
+            for (int i = 1; i < cols - 1; i++)
+                function += $",{columnNames[i]}";
+            function += ") = ";
+
+            bool firstconj = true;
+            for (int i = 0; i < rows; i++)
+            {
+                if (truthTable[i][cols - 1] == 1)
+                {
+                    if (!firstconj)
+                        function += " ˅ ";
+
+                    bool firstparam = true;
+                    for (int j = 0; j < cols - 1; j++)
+                    {
+                        if (truthTable[i][j] == 1)
+                        {
+                            if (!firstparam) function += "˄";
+                            function += columnNames[j];
+                            firstparam = false;
+                        }
+                        else
+                        {
+                            if (!firstparam) function += "˄";
+                            function += $"¬{columnNames[j]}";
+                            firstparam = false;
+                        }
+                    }
+                    firstconj = false;
+                }
+            }
+
+            textBox.Text = function;
+            textBox.Select(0, 0);
 
             return form;
         }
